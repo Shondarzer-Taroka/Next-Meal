7f4d1648b84e919dda3b2f1eabf54fb70710283c
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-07-29 16:11:48 +0600

    time added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-// const getTime=async()=>{
-//     const res= await fetch('http://localhost:3000/time',{cache:'no-cache'})
-//     const data=await res.json()
-//     return data.currentTime
-// }
+const getTime=async()=>{
+    const res= await fetch('https://next-meal-five.vercel.app/time',{cache:'no-cache'})
+    const data=await res.json()
+    return data.currentTime
+}
 
 const page = async() => {
 
-//   let time=await getTime()
+  let time=await getTime()
     return (
         <div>
             <h1>About Page</h1>
-            {/* <h1>Time:{time}</h1> */}
+            <h1>Time:{time}</h1>
         </div>
     );
 };
